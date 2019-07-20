efea25f1136c6f21f203c74d082b7046794a5cc5
Dustin Myers <dustinmyers@users.noreply.github.com>
2019-07-20 14:52:34 -0500

    Update Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,68 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class Movie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
-
-  componentDidMount() {
-    // change this line to grab the id passed on the URL
+const Movie = (props) => {
+  const [movie, setMovie] = useState();
+ 
+  useEffect(() => {
     const id = 1;
-    this.fetchMovie(id);
+    // change ^^^ that line and grab the id from the URL
+    // You will NEED to add a dependency array to this effect hook
+
+    fetchMovie = id => {
+      axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(response => {
+          setMovie(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+  });
+
+  if (!movie) {
+    return <div>Loading movie information...</div>;
   }
 
-  fetchMovie = id => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
-        this.setState(() => ({ movie: response.data }));
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-  // Uncomment this code when you're ready for the stretch problems
-  // componentWillReceiveProps(newProps){
-  //   if(this.props.match.params.id !== newProps.match.params.id){
-  //     this.fetchMovie(newProps.match.params.id);
-  //   }
-  // }
-
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
-
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
-
-    const { title, director, metascore, stars } = this.state.movie;
-    return (
-      <div className="save-wrapper">
-        <div className="movie-card">
-          <h2>{title}</h2>
-          <div className="movie-director">
-            Director: <em>{director}</em>
-          </div>
-          <div className="movie-metascore">
-            Metascore: <strong>{metascore}</strong>
-          </div>
-          <h3>Actors</h3>
-
-          {stars.map(star => (
-            <div key={star} className="movie-star">
-              {star}
-            </div>
-          ))}
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-        <div className="save-button">Save</div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
       </div>
-    );
-  }
+      <div className="save-button">Save</div>
+    </div>
+  );
 }
