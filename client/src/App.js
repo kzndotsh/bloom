c890a819f37b410cf46f4512257fa1d44019ba18
Donald Whitely <dswhitely1@gmail.com>
2019-07-21 14:32:14 -0400

    Updated App.JS and SavedList.JS to removed Class Component References

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
 
 const App = () => {
-  const [savedList, setSaveList] = useState([]);
-  
-  addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+  const [savedList, setSavedList] = useState( [] );
+
+  const addToSavedList = movie => {
+    setSavedList( [...savedList, movie] );
   };
 
-  render() {
-    return (
-      <div>
-        <SavedList list={savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <SavedList list={savedList} />
+      <div>Replace this Div with your Routes</div>
+    </div>
+  );
+};
 
 export default App;
