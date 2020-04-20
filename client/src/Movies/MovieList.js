3693b2ca00ea6978b456f4fa1c5c9fabbb0caa6a
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-20 11:43:40 +0200

    No AJAX inside movielist, the parent holds the state

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovies(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    
-    getMovies();
-  }, []);
-  
   return (
     <div className="movie-list">
-      {movies.map(movie => (
+      {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
