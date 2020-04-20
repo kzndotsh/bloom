c16279bc5d563e77b4156dfd8458924d1b30ad76
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-20 11:25:36 +0200

    adhere to code style

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
