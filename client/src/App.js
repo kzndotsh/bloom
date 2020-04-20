fa4ca07805dd76a1500523ec432544db31ce2497
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-20 11:44:49 +0200

    List of movies held in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          setMovieList(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    getMovies();
+  }, []);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
