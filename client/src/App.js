1dd0ff931aa062f745bbefd5a0545579b54f49f1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-14 15:35:31 +0200

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
 
-const App = () => {
+export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
@@ -28,9 +28,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+
       <div>Replace this Div with your Routes</div>
     </div>
   );
-};
-
-export default App;
+}
--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// You'll need to wrap <App /> for routing to work
 ReactDOM.render(<App />, document.getElementById('root'));
