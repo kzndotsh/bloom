53201894203595e9a190c64f4665ecb1f50d8af6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-05 14:13:37 -0400

    adds package and readme bits.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header>
           Color Picker Sprint Challenge
           <a data-testid="logoutButton" href="#">logout</a>
-        </header> 
+        </header>
       </div>
     </Router>
   );
@@ -20,6 +20,6 @@ function App() {
 export default App;
 
 //Task List:
-//1. Add in two routes for the Login Component, one for the default path '/' and one for the '/login.'
+//1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
 //2. Render BubblePage as a PrivateRoute
 //2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
