0261d57b8dfaa9fafa244f121ff5d4c1d30d6ac0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-17 06:13:02 -0500

    adds to tests.

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -18,5 +18,5 @@ test('renders "Associated Press" when no author is given', ()=> {
 test('executes handleDelete when the delete button is pressed', ()=> {
 });
 
-//Task List:
+//Task List: 
 //1. Complete all above tests. Create test article data when needed.
