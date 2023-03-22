0a4551a66fdb6c59e9572402ccf35c43dc776e5e
kaizensh <admin@kaizen.wtf>
2023-03-22 16:57:35 -0400

    submit assignment

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
 
-const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
+const Comments = (props) => {
   const { comments } = props;
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((comment) => {
+        return <Comment comment={comment} key={comment.id} />;
+      })}
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,30 +1,35 @@
 // You do not need to change any code in this file for MVP
-import React from "react";
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
-import "./SearchBar.css";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from '@fortawesome/free-regular-svg-icons';
+import './SearchBar.css';
 
 const SearchBar = (props) => {
+  const changeHandler = (event) => {
+    props.setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="search-bar-wrapper">
-      <div className="social">
+    <div className='search-bar-wrapper'>
+      <div className='social'>
         <FontAwesomeIcon icon={faInstagram} />
       </div>
-      <form className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-        />
+      <form className='search-form'>
+        <input type='text' placeholder='Search' onChange={changeHandler} />
       </form>
-      <div className="social-wrapper">
-        <div className="social">
+      <div className='social-wrapper'>
+        <div className='social'>
           <FontAwesomeIcon icon={faCompass} />
         </div>
-        <div className="social">
+        <div className='social'>
           <FontAwesomeIcon icon={faHeart} />
         </div>
-        <div className="social">
+        <div className='social'>
           <FontAwesomeIcon icon={faCircle} />
         </div>
       </div>
