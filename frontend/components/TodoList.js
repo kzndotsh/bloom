5b87a21238b9f8d7762930718fc2a39dcd7205d9
kaizen <admin@kaizen.wtf>
2023-05-10 18:09:49 -0400

    submit project

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
 export default class TodoList extends React.Component {
+  constructor() {
+    super();
+  }
+
   render() {
-    return null
+    return (
+      <div className='todo-list'>
+        <h2>Todo List</h2>
+        {this.props.todos.map((todo) => {
+          return (
+            <Todo
+              todo={todo}
+              key={todo.id}
+              toggleTodo={this.props.toggleTodo}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-import './styles/reset.css'
-import './styles/styles.css'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './styles/reset.css';
+import './styles/styles.css';
 
 render(
   <React.StrictMode>
-    <h1>Todo App AJAX</h1>
     <App />
-  </React.StrictMode>
-  , document.getElementById('root')
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
