6d68d478f4194bd71254f396863db9168527b6f8
kaizen <admin@kaizen.wtf>
2023-05-30 22:44:08 -0400

    submit project

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const CalcButton = (props) => {
-    const { value, onClick = (e)=>{}, size = 4} = props;
+  const { value, onClick = (e) => {}, size = 4 } = props;
 
-    return(<div className={`col-xs-${size}`}>
-        <button value={value} type="button" onClick={(e)=>{onClick(e)}} className="btn">{value}</button>
-    </div>);
-}
+  return (
+    <div className={`col-xs-${size}`}>
+      <button
+        value={value}
+        type='button'
+        onClick={(e) => {
+          onClick(e);
+        }}
+        className='btn'
+      >
+        {value}
+      </button>
+    </div>
+  );
+};
 
 export default CalcButton;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 import './style.css';
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+render(<App />, document.getElementById('root'));
