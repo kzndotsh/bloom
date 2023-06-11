64746a983913da2d8c0dc78279b862fa6d2ecee5
kaizen <admin@kaizen.wtf>
2023-06-11 18:07:10 -0400

    finish guided project

---
+++ followAlong/src/components/Families.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useContext } from 'react';
+import '../styles.scss';
+import { FamilyContext } from '../App';
+
+const Families = () => {
+    const familyContext = useContext(FamilyContext);
+    console.log(familyContext);
+    return (
+        <section className='header'>
+            <h1>Family Trees</h1>
+            {familyContext.families.map((family) => (
+                <button
+                    className={`family-button ${
+                        family.familyName ===
+                            familyContext.activeFamily.familyName && 'active'
+                    }`}
+                    key={family.familyName}
+                    onClick={() => familyContext.setAc tiveFamily(family)}>
+                    {family.familyName}
+                </button>
+            ))}
+        </section>
+    );
+};
+
+export default Families;
