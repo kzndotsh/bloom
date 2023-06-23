822b1237a6015d51060bba4ecd341021af0b1154
kaizen <admin@kaizen.wtf>
2023-06-23 08:18:01 -0400

    submit project

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Episode from './Episode';
 
 export default function Episodes(props) {
-  return (
-    <div data-testid="episodes-container" className="episodes">
-      {props.episodes.map(episode => <Episode episode={episode} />)}
-    </div>
-  );
+    return (
+        <div
+            data-testid='episodes-container'
+            className='episodes'>
+            {props.episodes.map((episode) => (
+                <Episode episode={episode} />
+            ))}
+        </div>
+    );
 }
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,28 +1,37 @@
-import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Episode from "./../Episode";
-
-test("renders without error", () => {});
-
-test("renders the summary test passed as prop", () => {});
-
-test("renders default image when image is not defined", () => {});
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Episode from './../Episode';
 
 // ----- EXAMPLE EPISODE TEST OBJECT -----
-// const exampleEpisodeData = {
-//   airdate: "2016-07-15",
-//   airstamp: "2016-07-15T12:00:00+00:00",
-//   airtime: "",
-//   id: 553946,
-//   image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
-//   name: "Chapter One: The Vanishing of Will Byers",
-//   number: 1,
-//   rating: { average: 8.2 },
-//   runtime: 49,
-//   season: 1,
-//   summary:
-//     "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
-//   type: "regular",
-//   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-// };
+const exampleEpisodeData = {
+    id: 553946,
+    image: 'https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg',
+    name: 'Chapter One: The Vanishing of Will Byers',
+    number: 1,
+    runtime: 49,
+    season: 1,
+    summary:
+        "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
+};
+
+test('renders without error', () => {
+    render(<Episode episode={exampleEpisodeData} />);
+});
+
+test('renders the summary test passed as prop', () => {
+    render(<Episode episode={exampleEpisodeData} />);
+    const summary = screen.getByText(exampleEpisodeData.summary);
+    expect(summary).toBeInTheDocument();
+});
+
+test('renders default image when image is not defined', () => {
+    const episodeData = { ...exampleEpisodeData, image: null };
+    render(<Episode episode={episodeData} />);
+    const defaultImage = screen.getByAltText(
+        'https://i.ibb.co/2FsfXqM/stranger-things.png'
+    );
+    expect(defaultImage).toBeInTheDocument();
+});
+
+export default exampleEpisodeData;
