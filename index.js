7537112ada9e5d7f9fdefe393a8ffc8da20cb458
kaizen <admin@kaizen.wtf>
2023-07-18 12:16:27 -0400

    complete assignment

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () =>
+  console.log('\nServer is running on http://localhost:9000\n')
+);
