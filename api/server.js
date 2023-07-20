2636a1bde66aff078dab82fafe9dea5029e0e2a7
kaizen <admin@kaizen.wtf>
2023-07-20 16:59:47 -0400

    submit project

--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
-// implement your server here
-// require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const postsRouter = require('./posts/posts-router');
+
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+        <h2>Posts API</h2>
+        <p>Welcome to the Posts API</p>
+    `);
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// require your server and launch it here
+const server = require('./api/server');
+
+server.listen(4000, () => {
+  console.log('Server listening on port 4000');
+});
